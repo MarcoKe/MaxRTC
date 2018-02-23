accb29fc516fa61973c0187c19800a02302160bf
Marco Kemmerling <marco@kemmerling.me>
2018-02-23 13:13:20 +0100

    changes ilp creation from object based to string/file based

---
+++ cplexutil/LPWriter.java
@@ -0,0 +1,74 @@
+package cplexutil;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LPWriter {
+	private boolean max = true; 
+	private String objective = ""; 
+	private String constraints = ""; 
+	private int numConstraints = 0; 
+	private String integerVars = ""; 
+	
+	public void addObjective(boolean max, String obj) {
+		this.max = max; 
+		this.objective += obj;
+	}
+	
+	public void addConstraint(String constraint) {
+		constraints += " c" + (numConstraints+1) + ": " + constraint + "\n";
+		numConstraints++; 
+	}
+	
+	public void addBound() {
+		
+	}
+	
+	public void addIntegerVars(boolean binary, String vars) {
+		if (binary) 
+			integerVars += "Binaries \n"; 
+		else 
+			integerVars += "Generals \n"; 
+		
+		integerVars += " " + vars + "\n";
+		
+	}
+	
+	public String toString() {
+		// objective 
+		String program = ""; 
+		if (max) 
+			program += "Maximize \n"; 
+		else 
+			program += "Minimize \n"; 
+		
+		program += " obj: " + objective + "\n"; 
+		
+		// constraints 
+		program += "Subject To \n";
+		program += constraints; 
+		
+		// bounds 
+		
+		// integer vars 
+		program += integerVars;
+		
+		
+		program += "End";
+		
+		return program; 
+	}
+	
+	public void writeFile(String name) throws IOException {
+		    BufferedWriter writer = new BufferedWriter(new FileWriter(name));
+		    writer.write(this.toString());		     
+		    writer.close();		
+	}
+	
+	public static void main(String[] args) {
+		String s = "testest"; 
+		System.out.println(s.substring(0, s.length()-2));
+	}
+
+}
--- maxrtc/ILPSolver.java
+++ maxrtc/ILPSolver.java
@@ -1,141 +1,109 @@
 package maxrtc;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
+import cplexutil.LPWriter;
 import data.PhylogeneticTree;
 import data.RootedTriplet;
 import ilog.concert.IloException;
-import ilog.concert.IloIntVar;
-import ilog.concert.IloLinearIntExpr;
-import ilog.concert.IloMPModeler;
 import ilog.cplex.IloCplex;
 
 public class ILPSolver {
 	boolean tripletIndicator[][][]; 
-	
+	int numTaxa; 
+
 	public ILPSolver(List<RootedTriplet> triplets, int numTaxa) {
 		this.tripletIndicator = new boolean[numTaxa][numTaxa][numTaxa]; 
-		
+		this.numTaxa = numTaxa; 
+
 		for (RootedTriplet t : triplets) {
 			this.tripletIndicator[t.a][t.b][t.c] = true; 
 		}
-		
+
 	}
-	
-	
+
 	
 	public void solve() {
-        try {
-        	IloCplex cplex = new IloCplex();
-        	
-        	IloMPModeler model = cplex; 
-        	int numVars = tripletIndicator.length * tripletIndicator.length * tripletIndicator.length; 
-    		int lb = 0;
-            int ub = 1;
-            String[]    varname = {"x1", "x2", "x3"};
-            
-            IloIntVar[][][] x = new IloIntVar[tripletIndicator.length][tripletIndicator.length][tripletIndicator.length];
-            IloLinearIntExpr obj = model.linearIntExpr();
+		writeILP(); 
+		IloCplex cplex;
+		try {
+			cplex = new IloCplex();
+			cplex.importModel("ilp.lp");
+			cplex.solve(); 
+			cplex.writeSolution("test.sol");
 
-            
-            for (int i = 0; i < x.length; i++) {
-            	for (int j = 0; j < x[0].length; j++) {
-            		for (int k = 0; k < x[0][0].length; k++) {
-            			x[i][j][k] = model.intVar(lb, ub, "t"+Integer.toString(i)+","+Integer.toString(j)+";"+Integer.toString(k)); 
-            			
-            			if (tripletIndicator[i][j][k])
-            				obj.addTerm(1, x[i][j][k]);
-            			
-            			
-            		}
-            	}
-            }
-            
-            model.addMaximize(obj); 
-
-            
-            // constraints 
-            for (int i = 0; i < x.length; i++) {
-            	for (int j = 0; j < x[0].length; j++) {
-            		for (int k = 0; k < x[0][0].length; k++) {
-            			if (!(i == j || i == k 
-        						|| j == k)) {
-            				model.addEq(model.sum(x[i][j][k], x[i][k][j], x[j][k][i]), 1);
-            			}
-            		}
-            	}
-            }
-            
-            for (int i = 0; i < x.length; i++) {
-            	for (int j = 0; j < x[0].length; j++) {
-//            		if (i == j) continue; 
-            		for (int k = 0; k < x[0][0].length; k++) {
-//            			if (i == k || j == k) continue; 
-            			for (int l = 0; l < x.length; l++) {
-//            				if (i == l || j == l || k == l) continue; 
-            				if (!(i == j || i == k || i == l
-            						|| j == k || j == l
-            						|| l == k)) {
-            					model.addLe(model.sum(x[i][j][k], x[j][k][l], model.negative(x[i][k][l])), 1);
-                				model.addLe(model.sum(x[i][j][k], x[j][k][l], model.negative(x[i][j][l])), 1); 
-                				model.addLe(model.sum(x[i][j][l], x[j][k][l], model.negative(x[i][k][l])), 1); 
-                				model.addLe(model.sum(x[i][j][l], x[i][k][l], model.negative(x[j][k][l])), 1);
-                			
-            					
-            				}
-            			}
-            		}
-            	}
-            }
-            
-            cplex.exportModel("lpex1.lp");
-
-            
-            if ( cplex.solve() ) {
-//                double[][][] sol     = cplex.getValues(x);
-//                double[] dj    = cplex.getReducedCosts(var[0]);
-//                double[] pi    = cplex.getDuals(rng[0]);
-//                double[] slack = cplex.getSlacks(rng[0]);
-//
-//                cplex.output().println("Solution status = " + cplex.getStatus());
-//                cplex.output().println("Solution value  = " + cplex.getObjValue());
-//
-//                int nvars = x.length;
-//                for (int j = 0; j < nvars; ++j) {
-//                   cplex.output().println("Variable " + j +
-//                                          ": Value = " + x[j] +
-//                                          " Reduced cost = " + dj[j]);
-//                }
-//
-//                int ncons = slack.length;
-//                for (int i = 0; i < ncons; ++i) {
-//                   cplex.output().println("Constraint " + i +
-//                                         ": Slack = " + slack[i] +
-//                                         " Pi = " + pi[i]);
-//                }
-             }
-            
-            
-        	
-        	
-        	cplex.end(); 
-        }
-        
-        catch (IloException exc) {
-            System.err.println("Concert exception '" + exc + "' caught");
-         }
-
+		} catch (IloException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	public void populate(IloMPModeler model) {
+
+	// builds ilp and writes to file 
+	public void writeILP() {
+		LPWriter lpwriter = new LPWriter(); 
+
+		String obj = "";
+		String binaries = ""; 
+		for (int i = 0; i < numTaxa; i++) {
+			for (int j = 0; j < numTaxa; j++) {
+				for (int k = 0; k < numTaxa; k++) {
+
+					if (tripletIndicator[i][j][k]) {
+						obj += "t"+i+j+k + " + ";
+						binaries += "t"+i+j+k + " ";
+					}
+
+				}
+			}
+		}
+
+		obj = obj.substring(0, obj.length()-3);
+		lpwriter.addObjective(true, obj);
+		lpwriter.addIntegerVars(true, binaries);  
+
+		// constraints 
+		for (int i = 0; i < numTaxa; i++) {
+			for (int j = 0; j < numTaxa; j++) {
+				for (int k = 0; k < numTaxa; k++) {
+					if (!(i == j || i == k 
+							|| j == k)) {
+						lpwriter.addConstraint("t"+i+j+k + " + t" + i+k+j + " + t" + j+k+i + " = 1" );
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < numTaxa; i++) {
+			for (int j = 0; j < numTaxa; j++) {
+				for (int k = 0; k < numTaxa; k++) {
+					for (int l = 0; l < numTaxa; l++) {
+						if (!(i == j || i == k || i == l
+								|| j == k || j == l
+								|| l == k)) {
+							lpwriter.addConstraint("t" + i+j+k + " + t" +j+k+l + " - t" + i+k+l + " <= 1");
+							lpwriter.addConstraint("t" + i+j+k + " + t" +j+k+l + " - t" + i+j+l + " <= 1");
+							lpwriter.addConstraint("t" + i+j+l + " + t" +j+k+l + " - t" + i+k+l + " <= 1");
+							lpwriter.addConstraint("t" + i+j+l + " + t" +i+k+l + " - t" + j+k+l + " <= 1");
+						}
+					}
+				}
+			}
+		}   
 		
-       
+		try {
+			lpwriter.writeFile("ilp.lp");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
+
 	public static void main(String[] args) {
-		PhylogeneticTree t = new PhylogeneticTree(new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
+		PhylogeneticTree t = new PhylogeneticTree(new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8)));
 		t.addEdge(0, 1);
 		t.addEdge(0, 2);
 		t.addEdge(1, 3);
@@ -144,7 +112,7 @@ public class ILPSolver {
 		t.addEdge(2, 6);
 		t.addEdge(3, 7);
 		t.addEdge(3, 8);
-		
+
 		ILPSolver solver = new ILPSolver(t.findAllTriplets(), 9);
 		solver.solve();
 	}
